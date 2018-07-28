22a03908ff57c78a88caac452839347c694af5ac
Coen Valk <clcjvalk@gmail.com>
2018-07-27 21:54:31 -0400

    add egohand dataset acknowledgement

--- create_dataset.py
+++ create_dataset.py
@@ -25,9 +25,17 @@ SOFTWARE.
 """
 Acknowledgements:
 
-[1] A. Mittal, A. Zisserman, P. H. S. Torr
+A. Mittal, A. Zisserman, P. H. S. Torr
 Hand detection using multiple proposals  
 British Machine Vision Conference, 2011 
+
+@InProceedings{Bambach_2015_ICCV,
+author = {Bambach, Sven and Lee, Stefan and Crandall, David J. and Yu, Chen},
+title = {Lending A Hand: Detecting Hands and Recognizing Activities in Complex Egocentric Interactions},
+booktitle = {The IEEE International Conference on Computer Vision (ICCV)},
+month = {December},
+year = {2015}
+}
 
 """
 
